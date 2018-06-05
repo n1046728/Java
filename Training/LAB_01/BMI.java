802c9b39c5b722da08cabea8511f634b18d8b074
JamesSyu <N1046728@ntub.edu.tw>
2018-06-06 00:16:36 +0800

    Add files via upload

--- Training/LAB_01/BMI.java
+++ Training/LAB_01/BMI.java
@@ -2,15 +2,15 @@
 public class BMI {
 	public static void main(String[] args){
 		//{{height...},{weight...}}
-		double [][] src = new double[][]{{175.5,168,178.5,181},{73.2,60.3,78.8,88.6}};
+		double [][] src=new double[][]{{175.5,168,178.5,181},{73.2,60.3,78.8,88.6}};
 		
 		//calculate BMI 
-		//BMI formula=kg/m^2
+		//BMI formula= kg/m^2
 		double h=0;
 		double w=0;
-		for(int i=0;i<source[0].length;i++){
-			h=source[0][i]/100;
-			w=source[1][i];
+		for(int i=0;i<src[0].length;i++){
+			h=src[0][i]/100;
+			w=src[1][i];
 			System.out.println(String.format("height:%.2f , weight:%.2f , BMI:%.2f",
 								h,w,w/Math.pow(h, 2)));
 		}
