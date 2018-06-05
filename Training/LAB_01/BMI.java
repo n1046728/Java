dbec2601f3333c823d9a3c2063faa188b678d24b
JamesSyu <N1046728@ntub.edu.tw>
2018-06-06 00:11:38 +0800

    Update BMI.java

--- Training/LAB_01/BMI.java
+++ Training/LAB_01/BMI.java
@@ -2,10 +2,10 @@
 public class BMI {
 	public static void main(String[] args){
 		//{{height...},{weight...}}
-		double [][] source=new double[][]{{175.5,168,178.5,181},{73.2,60.3,78.8,88.6}};
+		double [][] src = new double[][]{{175.5,168,178.5,181},{73.2,60.3,78.8,88.6}};
 		
 		//calculate BMI 
-		//BMI formula kg/m^2
+		//BMI formula=kg/m^2
 		double h=0;
 		double w=0;
 		for(int i=0;i<source[0].length;i++){
