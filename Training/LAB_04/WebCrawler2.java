f7799caaaed3e61181862b61979b7f51907e49fd
JamesSyu <N1046728@ntub.edu.tw>
2018-06-12 09:51:48 +0800

    revise scanner and delete method getPM25ByArea

--- Training/LAB_04/WebCrawler2.java
+++ Training/LAB_04/WebCrawler2.java
@@ -1,58 +1,47 @@
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class WebCrawler2 {
     public static void main(String [] args) throws IOException{
-        Map<String,String> map= getMap();
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println(scanner.next());
+    	Document doc = Jsoup.connect("https://taqm.epa.gov.tw/pm25/tw/PM25A.aspx?area=10").get();
+    	Map<String,String> map= getMap();
+
         while(true){
-            System.out.println("請輸入以下其中一個區域查詢PM2.5，不輸入按ENTER可離開");
+            System.out.println("請輸入以下其中一個區域查詢PM2.5，輸入非下述區域會離開...");
             System.out.println(map.keySet());
+            System.out.print("請輸入：");
             Scanner scanner = new Scanner(System.in);
-            System.out.println(scanner.next());
             String input = scanner.next();
-            if(input.isEmpty()){
-               break; 
+            
+            if(map.containsKey(input)){
+            	System.out.println("find pm2.5中...請稍候");
+                System.out.println(input+"現在的PM2.5即時濃度為："+doc.select("#"+map.get(input)).text());
+                System.out.println();
             }else{
-                System.out.println(input+"現在的PM2.5為："+getPM25ByArea(input));
-                input=null;
+            	System.out.println("離開...");
+            	break; 
             }
         }
     }
-    public static String getPM25ByArea(String area) throws IOException{
-        String pm =null;
-        Map<String,String> map= getMap();
-        Document doc = Jsoup.connect("https://taqm.epa.gov.tw/pm25/tw/PM25A.aspx?area=10").get();
-//        for (Entry<String, String> entry : map.entrySet()) {
-//            System.out.print(entry.getKey()+"\t");
-//            System.out.println(entry.getValue());
-//        } 
-        pm = doc.select("#"+map.get(area)).text();
-//        System.out.println(doc.elect("#ctl08_gv_ctl04_lab1").text());
-//        System.out.println(map.get("萬里")+":"+doc.select("#"+pm).text());
-//        System.out.println(pm);
-//        System.out.println(doc.select(pm).text());
-        
-        return pm;
-    }
-    
+ 
+    //將地區名稱與對應的即時濃度css id做成map供查找對映
     public static Map<String,String> getMap() throws IOException{
         Map<String,String> map =new HashMap<>();
         Document doc = Jsoup.connect("https://taqm.epa.gov.tw/pm25/tw/PM25A.aspx?area=10").get();
         for(int i =2;i<=99;i++){
+        	//area css_id
+            //<a id="ctl08_gv_ctl02_linkSite" href="http://taqm.epa.gov.tw/taqm/tw/Site/Keelung.aspx" target="_blank">基隆</a>
             String text = doc.select("#ctl08_gv_ctl"+String.format("%02d", i)+"_linkSite").text();
-            //System.out.println("#ctl08_gv_ctl"+String.format("%02d", i)+"_linkSite");
             if(!text.isEmpty()){
+            	//pm2.5 value
+            	//<span id="ctl08_gv_ctl02_lab1">26</span>
                 map.put(text,"ctl08_gv_ctl"+String.format("%02d", i)+"_lab1");
             }     
         }
         return map;
     }
 }
-
