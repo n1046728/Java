100432c48208ecd2c43689af380f10de39e66cd4
JamesSyu <N1046728@ntub.edu.tw>
2018-06-19 09:44:08 +0800

    Update WebCrawler.java

--- Training/LAB_04/WebCrawler.java
+++ Training/LAB_04/WebCrawler.java
@@ -9,15 +9,18 @@ public class WebCrawler {
         Document doc = Jsoup.connect("https://tw.stock.yahoo.com/q/q?s=2317").get();
         //System.out.println(doc.select("td[nowrap]").html());
         //System.out.println(doc.select("td[nowrap]").outerHtml());
+        //find td tag has nowrap attribute 
         System.out.println(doc.select("td[nowrap]").text());
         String [] dataArray = doc.select("td[nowrap]").text().split(" ");
+        //to show strike price
         System.out.println(dataArray[2]);
+        //using function to esay get strike price
         System.out.println("2330:"+getStkPrice("2330"));
         System.out.println("2498:"+getStkPrice("2498"));
         System.out.println(String.format("%02d", 1));
     }
-  
-    public static String getStkPrice(String symbol) throws IOException{   
+    
+    public static String getStkPrice(String symbol) throws IOException{
         Document doc = Jsoup.connect("https://tw.stock.yahoo.com/q/q?s="+symbol).get();
         String [] dataArray = doc.select("td[nowrap]").text().split(" ");
         String stkPrice = dataArray[2];
