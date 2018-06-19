baba8d0fc36a69916bc01cb7db1f3e0244fe8c3b
JamesSyu <N1046728@ntub.edu.tw>
2018-06-19 13:58:15 +0800

    Update JsonTest.java

--- Training/LAB_03/JsonTest.java
+++ Training/LAB_03/JsonTest.java
@@ -5,30 +5,6 @@ import com.google.gson.Gson;
 
 public class JsonTest {
 	public static void main(String args [] ) throws Exception{
-		/* Note 20180601
-		 * 		Introducing JSON: www.json.org
-		 * 		select java->google-gson
-		 * 		
-		 * 		傳輸F.F(flate file)純文字->xml->json		
-		 * 
-		 * 		add gson.jar
-		 * 			right-clicking on the Project → Build Path → Configure Build Path. Under Libraries tab, 
-		 * 			click Add Jars or Add External JARs and give the Jar.
-		 *	
-		 *		天氣數據openweathermap.org
-		 *			search taipei  
-		 *			API->JSON->otherfeathers
-		 *		
-		 *		install chrome jsonView
-		 *
-		 *		google 行政院農委會JSON->食安飲食->check json ->市售米不合格->資料介接	
-		 * 		gooele UBike json->使用資料->資料存取網址->更改副檔名.json
-		 * 		google 行政院農委會JSON->農業旅遊->check json ->伴手禮->資料介接
-		 * 
-		 * 		自己補充：	
-		 * 			GoogleGsonTutorial: https://howtodoinjava.com/apache-commons/google-gson-tutorial-convert-java-object-to-from-json/
-		 * 			github: https://github.com/google/gson
-		 */			
 		URL url= new URL("http://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx");
 		InputStreamReader reader = new InputStreamReader(url.openStream());
 		System.out.println(reader.ready());
