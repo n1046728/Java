87eb309129628966f153c9934122ef06effe8087
JamesSyu <N1046728@ntub.edu.tw>
2018-06-19 10:08:47 +0800

    Update temp.java

--- Training/LAB_05/temp.java
+++ Training/LAB_05/temp.java
@@ -15,10 +15,12 @@ public class PM25LAB {
             for(int i=1;i<=27;i++){
                 String areaCssQuery =String.format("#ctl08_gv_ctl%02d_linkSite", i);
                 String pm25CssQuery =String.format("#ctl08_gv_ctl%02d_lab1", i);
+                //i若不足兩位數則補零 ex. i=1 => 01
                 //System.out.println(String.format("%02d", i));
                 String area= doc.select(areaCssQuery).text().trim();
                 String pm25= doc.select(pm25CssQuery).text().trim();
                 if(!area.equals("")){
+                    //handle pm2.5 value when pm2.5 is ND and empty
                     pm25Map.put(area,pm25.equals("ND")|pm25.equals("")?-1:Integer.parseInt(pm25));
                 }
             }
