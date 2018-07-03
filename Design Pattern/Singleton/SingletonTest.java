106a13d9d4750feb582016ac5c2bfd22e11ce0fe
JamesSyu <N1046728@ntub.edu.tw>
2018-07-03 14:33:13 +0800

    Update SingletonTest.java

--- Design Pattern/Singleton/SingletonTest.java
+++ Design Pattern/Singleton/SingletonTest.java
@@ -1,1 +1,12 @@
-
+public class SingletonTest {
+    public static void main(String [] args){
+//        Transaction t1 = new Transaction();
+//        t1.submit(1_0000);
+        
+        Transaction t2 = Transaction.getTransaction();
+        t2.submit(2_0000);
+        
+        Transaction t3 = Transaction.getTransactionFromPool();
+        t3.submit(3_0000);
+    }
+}
